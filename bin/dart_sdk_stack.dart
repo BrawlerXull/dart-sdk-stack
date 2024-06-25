4af500d21834a45eeb84ff71269f01edc4d56c1c
Chinmay <chaudhari.chinmay12345@gmail.com>
2024-06-26 00:55:13 +0530

    Initialised the project

---
+++ bin/dart_sdk_stack.dart
@@ -0,0 +1,5 @@
+import 'package:dart_sdk_stack/dart_sdk_stack.dart' as dart_sdk_stack;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_sdk_stack.calculate()}!');
+}
---
+++ test/dart_sdk_stack_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_sdk_stack/dart_sdk_stack.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
